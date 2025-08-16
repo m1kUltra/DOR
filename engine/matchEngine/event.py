605a43b3a65a57e2248a1ff93892582f17a9a6cf
kellyd92 <kellyd92@tcd.ie>
2025-08-16 07:27:50 +0100

    Force push latest save script and updates

---
+++ engine/matchEngine/event.py
@@ -0,0 +1,16 @@
+"""event_controller"""
+"""event occured overwrites and changes states regardless of ball_status
+has the role of being the refarees whistle to send us to the next state
+
+try_given penalty_given fk_given, scrum_given, penalty_try_given, 
+Ball determines lineout already
+
+penalty_given is a shell that then runs set_piece choices and receives there pick of what to do that will change the next state,
+either something for nudeges or scrum
+
+
+
+
+
+
+"""
